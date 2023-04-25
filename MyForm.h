ffafc864fbda92712e0a13779984331beb3f907d
moazOS <moaz.zekry3@gmail.com>
2023-04-25 19:24:39 +0200

    2a5r 7aga

--- MyForm.h
+++ MyForm.h
@@ -1,5 +1,12 @@
 #pragma once
+#include <iostream>
 #include "masseg.h"
+#include <stack>
+#include <list> 
+#include <string>
+#include <msclr/marshal_cppstd.h>
+#include <msclr/marshal.h>
+
 namespace Project9 {
 
 	using namespace System;
@@ -8,7 +15,9 @@ namespace Project9 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace std;
 	
+
 
 
 	/// <summary>
@@ -38,8 +47,9 @@ namespace Project9 {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Label^ label1;
+
 	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ Myfavorit;
 	protected:
 
 	private:
@@ -56,45 +66,45 @@ namespace Project9 {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->Myfavorit = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(270, 303);
+			this->button1->Location = System::Drawing::Point(584, 563);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(201, 28);
+			this->button1->Size = System::Drawing::Size(91, 28);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"send";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(746, 9);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
-			this->label1->TabIndex = 1;
-			this->label1->Text = L"label1";
-			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
-			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(300, 377);
+			this->textBox1->Location = System::Drawing::Point(12, 568);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(217, 20);
+			this->textBox1->Size = System::Drawing::Size(566, 20);
 			this->textBox1->TabIndex = 2;
 			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// Myfavorit
+			// 
+			this->Myfavorit->Location = System::Drawing::Point(584, 537);
+			this->Myfavorit->Name = L"Myfavorit";
+			this->Myfavorit->Size = System::Drawing::Size(91, 26);
+			this->Myfavorit->TabIndex = 3;
+			this->Myfavorit->Text = L"Myfavorit";
+			this->Myfavorit->UseVisualStyleBackColor = true;
+			this->Myfavorit->Click += gcnew System::EventHandler(this, &MyForm::Myfavorit_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(855, 600);
+			this->Controls->Add(this->Myfavorit);
 			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
@@ -110,17 +120,16 @@ namespace Project9 {
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) 
 	{
 	
-		masseg b(textBox1->Text , y);
+		masseg b(textBox1->Text , y );
 
-
-		this->Controls->Add(b.text);
+		
 		this->Controls->Add(b.favorit);
 		
+		this->Controls->Add(b.deleteFromeFavorit);
+		this->Controls->Add(b.text);
+		y += 3;
 
 
-
-
-		y+=3;
 	}
 
 
@@ -128,6 +137,12 @@ namespace Project9 {
 	}
 	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
+private: System::Void Myfavorit_Click(System::Object^ sender, System::EventArgs^ e) {
+	String^ ID = gcnew String(lstack.back().c_str());
+	MessageBox::Show(ID);
+	
+}
 };
 }
+
 
--- masseg.h
+++ masseg.h
@@ -1,45 +1,84 @@
-#include"MyForm.h"
-using namespace System;
+#include <iostream>
+#include <vector>
+#include <list> 
+#include <string>
+#include <msclr/marshal_cppstd.h>
+#include <msclr/marshal.h>
 using namespace System;
 using namespace System::ComponentModel;
 using namespace System::Collections;
 using namespace System::Windows::Forms;
 using namespace System::Data;
 using namespace System::Drawing;
-public ref class masseg 
-{
 
 
-public:System::Windows::Forms::Button^ favorit;
-public:System::Windows::Forms::Label^ text;
+std::vector<std::string> lstack;
+	public ref class masseg
+	{
+		
+	    String^ resala;
+		public:System::Windows::Forms::Button^ favorit;
+		public:System::Windows::Forms::Button^ deleteFromeFavorit;
+		public:System::Windows::Forms::Label^ text;
+
+
+	public: masseg(String^ mass, int y )
+	{
+		
+		resala = mass;
+		text = (gcnew System::Windows::Forms::Label());
+		text->AutoSize = true;
+		text->Location = System::Drawing::Point(573, 9 * y);
+		text->Name = mass;
+		text->Size = System::Drawing::Size(35, 20);
+		text->TabIndex = 1;
+		text->Text = mass;
+		favorit = (gcnew System::Windows::Forms::Button());
+		favorit->Location = System::Drawing::Point(500, 9 * y);
+		favorit->Name = L"star";
+		favorit->Size = System::Drawing::Size(40, 20);
+		favorit->TabIndex = 0;
+		favorit->Text = L"star";
+		favorit->UseVisualStyleBackColor = true;
+		favorit->Click += gcnew System::EventHandler(this, &masseg::ADD);
+		deleteFromeFavorit = (gcnew System::Windows::Forms::Button());
+		deleteFromeFavorit->Location = System::Drawing::Point(485, 9 * y);
+		deleteFromeFavorit->Name = L"X";
+		deleteFromeFavorit->Size = System::Drawing::Size(15, 20);
+		deleteFromeFavorit->TabIndex = 0;
+		deleteFromeFavorit->Text = L"X";
+		deleteFromeFavorit->UseVisualStyleBackColor = true;
+		deleteFromeFavorit->Click += gcnew System::EventHandler(this, &masseg::remove);
+		deleteFromeFavorit->Hide();
 
 
 
-public: masseg (String^ mass , int y)
-{
-	
-	text = (gcnew System::Windows::Forms::Label());
-	text->AutoSize = true;
-	text->Location = System::Drawing::Point(573, 9 * y);
-	text->Name = mass;
-	text->Size = System::Drawing::Size(35, 20);
-	text->TabIndex = 1;
-	text->Text = mass;
-	favorit = (gcnew System::Windows::Forms::Button());
-	favorit->Location = System::Drawing::Point(500, 9 * y);
-	favorit->Name = L"star";
-	favorit->Size = System::Drawing::Size(35, 20);
-	favorit->TabIndex = 0;
-	favorit->Text = L"star";
-	favorit->UseVisualStyleBackColor = true;
-	
+	}
+	private: System::Void ADD(System::Object^ sender, System::EventArgs^ e)
+	{
+		msclr::interop::marshal_context context;
+		std::string sname = context.marshal_as<std::string>(resala);
+		favorit->Text = L"done";
+		lstack.push_back(sname);
+		deleteFromeFavorit->Show();
+	}
+	private: System::Void remove(System::Object^ sender, System::EventArgs^ e)
+	{
+		favorit->Text = L"star";
+		for (auto i = lstack.begin(); i != lstack.end(); ++i)
+
+		{
+			std::string j = *i;
+			String^ ID = gcnew String(j.c_str());
+			if (ID->Equals(resala))
+			{
+				lstack.erase(i);
+			}
+			
+		}
+		deleteFromeFavorit->Hide();
+
+	}
 
 
-}
-private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e)
-{
-
-}
-
-
-};
+	};
